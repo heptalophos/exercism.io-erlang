d37e72b2ddc8b482db4b621cb8f050cb2773ca6e
Zero Delta <zerodeltahedge@gmail.com>
2019-05-08 14:54:29 +0300

    leap solution

---
+++ leap/src/leap.erl
@@ -0,0 +1,9 @@
+-module(leap).
+
+-export([leap_year/1]).
+
+-spec leap_year(pos_integer()) -> boolean().
+leap_year(Y) when Y rem 400 =:= 0 -> true;
+leap_year(Y) when Y rem 100 =:= 0 -> false;
+leap_year(Y) when Y rem   4 =:= 0 -> true;
+leap_year(_) -> false.
