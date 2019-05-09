8749e75a6bebb438d53fa24702d65efbdf7cb744
Zero Delta <zerodeltahedge@gmail.com>
2019-05-09 12:55:17 +0300

    isogram solution

---
+++ isogram/src/isogram.erl
@@ -0,0 +1,12 @@
+-module(isogram).
+
+-export([is_isogram/1]).
+
+-spec is_isogram([char(),...]) -> boolean().
+is_isogram(Phrase) -> Cs = [C || C <- string:to_lower(Phrase), $a =< C, C =< $z],
+                      has_duplicates(lists:sort(Cs)) == false.
+
+-spec has_duplicates([any()]) -> boolean().
+has_duplicates([X, Y | _]) when X =:= Y -> true;
+has_duplicates([_ | Rest]) -> has_duplicates(Rest);
+has_duplicates([]) -> false.
