1358dac5442886652f936bad7d8138a735f4f56b
Zero Delta <zerodeltahedge@gmail.com>
2019-05-28 18:10:55 +0300

    refactor accumulate

--- accumulate/src/accumulate.erl
+++ accumulate/src/accumulate.erl
@@ -6,5 +6,12 @@
 accumulate(Fn, Ls) -> accumulate(Fn, Ls, []).
 
 -spec accumulate(fun((A) -> B), list(A), list(B)) -> list(B).
-accumulate(_, [], Acc)     -> lists:reverse(Acc);
+accumulate(_, [], Acc)     -> reverse(Acc);
 accumulate(Fn, [H|T], Acc) -> accumulate(Fn, T, [Fn(H)|Acc]).
+
+-spec reverse(list(A)) -> list(A).
+reverse(Ls) -> reverse(Ls, []).
+
+-spec reverse(list(A), list(A)) -> list(A).
+reverse([], Ls)    -> Ls;
+reverse([H|T], Ls) -> reverse(T, [H|Ls]).
