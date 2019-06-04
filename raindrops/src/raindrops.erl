705bdad99efc82b5b95d4534fb60598f41e5a249
Zero Delta <zerodeltahedge@gmail.com>
2019-06-04 12:58:52 +0300

    raindrops solution

---
+++ raindrops/src/raindrops.erl
@@ -0,0 +1,14 @@
+-module(raindrops).
+
+-export([convert/1]).
+
+-spec convert(integer()) -> string().
+convert(Number) -> 
+    case [Drop || {Factor, Drop} <- [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}], 
+                                    Number rem Factor =:= 0] 
+    of
+        [] -> 
+            integer_to_list(Number);
+        Ds -> 
+            lists:concat(Ds)
+    end.
