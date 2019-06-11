ae7dca4873a35fea9b41e08c8b45bde6a06710d5
Zero Delta <zerodeltahedge@gmail.com>
2019-06-11 18:17:16 +0300

    Update nucleotide_count.erl

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -2,18 +2,14 @@
 
 -export([count/2, nucleotide_counts/1]).
 
--type nucleotide() :: $A | $C | $G | $T.
--type strand() :: [nucleotide()].
--type nuc_count() :: {nucleotide(), non_neg_integer()}.
-
--spec valid(nucleotide()) -> nucleotide() | {error, atom()}.
+-spec valid([char()]) -> [char()] | {error, atom()}.
 valid(N) when N =:= "A" orelse N =:= "C" orelse N =:= "G" orelse N =:= "T" -> N;
 valid(_) -> erlang:error(invalid_nucleotide).
 
--spec count(strand(), nucleotide()) -> non_neg_integer().
+-spec count(string(), [char()]) -> non_neg_integer().
 count(Strand, Nucleotide) -> 
     string:length(lists:filter(fun(X) -> valid([X]) =:= Nucleotide end, Strand)).
 
--spec nucleotide_counts(strand()) -> [nuc_count()].
+-spec nucleotide_counts(string()) -> {[char()], non_neg_integer()}.
 nucleotide_counts(Strand) -> 
     lists:map(fun(X) -> {X, count(Strand, X)} end, ["A", "C", "G", "T"]).
