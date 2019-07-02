784a787aca80c89b2971df4a8217168d86cb2f50
Zero Delta <zerodeltahedge@gmail.com>
2019-07-03 01:45:10 +0300

    refactor sum-of-multiples to pass updated tests

--- sum-of-multiples/src/sum_of_multiples.erl
+++ sum-of-multiples/src/sum_of_multiples.erl
@@ -4,8 +4,18 @@
 
 -spec sum([non_neg_integer()], pos_integer()) -> non_neg_integer().
 sum(Factors, Limit) -> 
-    lists:foldr(fun (M, Sum) -> Sum + M end, 0, 
+    lists:foldl(fun (M, Sum) -> if 
+                                    M =:= 0 -> Sum; 
+                                    true -> Sum + M 
+                                end
+                end, 
+                0, 
                 [X || X <- lists:seq(1, Limit - 1),
-                 lists:any(fun (M) -> X rem M =:= 0 end, 
+                 lists:any(fun (M) -> if
+                                        X =:= 0 -> true; 
+                                        X rem M =:= 0 -> true;
+                                        true -> false 
+                                      end
+                            end, 
                  Factors)]
                ).
