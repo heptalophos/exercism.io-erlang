8e9107b5612e760db94ee985ff9d7513c76aecb2
Zero Delta <zerodeltahedge@gmail.com>
2019-07-03 16:51:47 +0300

    sum-of-multiples refactor

--- sum-of-multiples/src/sum_of_multiples.erl
+++ sum-of-multiples/src/sum_of_multiples.erl
@@ -2,11 +2,13 @@
 
 -export([sum/2]).
 
--spec sum([non_neg_integer()], pos_integer()) -> non_neg_integer().
+-spec sum(Factors::[non_neg_integer()], Limit::pos_integer()) -> 
+          non_neg_integer().
+
 sum(Factors, Limit) -> 
     lists:foldl(fun (M, Sum) -> Sum + M end, 
                 0, 
-                [X || X <- lists:seq(1, Limit - 1),
+                [X || X <- lists:seq(0, Limit - 1),
                  lists:any(fun (M) -> if
                                         X rem M =:= 0 -> true;
                                         X =:= 0 -> true;
