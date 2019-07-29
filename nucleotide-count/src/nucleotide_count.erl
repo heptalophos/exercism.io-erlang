d61cf88d2fcffa25eda7841b00ee572bf3f67bc5
Zero Delta <zerodeltahedge@gmail.com>
2019-07-29 17:51:20 +0300

    nucleotide-count refactor

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -7,13 +7,19 @@
 -type nuc_count() :: {nucleotide(), non_neg_integer()}.
 
 -spec valid(nucleotide()) -> nucleotide() | {error, atom()}.
-valid(N) when N =:= "A" orelse N =:= "C" orelse N =:= "G" orelse N =:= "T" -> N;
-valid(_) -> erlang:error(invalid_nucleotide).
+valid(N) when N =:= "A"; N =:= "C"; N =:= "G"; N =:= "T" -> 
+    N;
+valid(_) -> 
+    erlang:error(invalid_nucleotide).
 
 -spec count(strand(), nucleotide()) -> non_neg_integer().
 count(Strand, Nucleotide) -> 
-    string:length(lists:filter(fun(X) -> valid([X]) =:= Nucleotide end, Strand)).
+    string:length(lists:filter(fun(X) -> 
+                    valid([X]) =:= Nucleotide end, 
+                    Strand)).
 
 -spec nucleotide_counts(strand()) -> [nuc_count()].
 nucleotide_counts(Strand) -> 
-    lists:map(fun(X) -> {X, count(Strand, X)} end, ["A", "C", "G", "T"]).
+    lists:map(fun(X) -> 
+                {X, count(Strand, X)} end, 
+                ["A", "C", "G", "T"]).
