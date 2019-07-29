f968c4ae2a66c825bb98148384e62016eb25d21e
Zero Delta <zerodeltahedge@gmail.com>
2019-07-29 17:48:59 +0300

    armstrong-numbers refactor

--- armstrong-numbers/src/armstrong_numbers.erl
+++ armstrong-numbers/src/armstrong_numbers.erl
@@ -2,8 +2,12 @@
 
 -export([is_armstrong_number/1]).
 
--spec is_armstrong_number(N::non_neg_integer()) -> boolean().
+-spec is_armstrong_number(non_neg_integer()) -> boolean().
 is_armstrong_number(Number) when Number >= 0 -> 
-    Digits = [list_to_integer([Digit]) || Digit <- integer_to_list(Number)],
+    Digits = [list_to_integer([Digit]) 
+              || Digit <- integer_to_list(Number)],
     Len = length(Digits),
-    Number =:= lists:foldl(fun(D, Acc) -> Acc + round(math:pow(D, Len)) end, 0, Digits).
+    Number =:= lists:foldl(fun(D, Acc) -> 
+                Acc + round(math:pow(D, Len)) end, 
+                0, 
+                Digits).
