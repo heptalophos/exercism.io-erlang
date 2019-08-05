2354159353b99516c6a7139f75d4d13af04a9a64
Zero Delta <zerodeltahedge@gmail.com>
2019-08-05 16:55:00 +0300

    Update isbn_verifier.erl

--- isbn-verifier/src/isbn_verifier.erl
+++ isbn-verifier/src/isbn_verifier.erl
@@ -8,7 +8,9 @@ is_valid(Isbn) -> is_valid(Isbn, 10, 0).
 
 % auxiliary
 
--spec is_valid([char()], integer(), integer()) -> boolean().
+-spec is_valid([char()], integer(), integer()) -> 
+                boolean().
+
 is_valid([], 0, Sum)   -> 
     Sum rem 11 =:= 0;
 
@@ -18,7 +20,8 @@ is_valid([$X], 1, Sum) ->
 is_valid(_C, I, _Sum) when I < 0 -> 
     false;
 
-is_valid([C | Cs], I, Sum) when C >= $0 andalso C =< $9 ->
+is_valid([C | Cs], I, Sum) when C >= $0 andalso 
+                                C =< $9 ->
     is_valid(Cs, I - 1, Sum + (I * (C - $0)));
 
 is_valid([_C | Cs], I, Sum)  ->
