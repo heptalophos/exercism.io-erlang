a055e08b80b507521b8c56416a9117e62e42db83
Zero Delta <zerodeltahedge@gmail.com>
2019-09-11 22:59:07 +0300

    Update robot_simulator.erl

--- robot-simulator/src/robot_simulator.erl
+++ robot-simulator/src/robot_simulator.erl
@@ -3,6 +3,9 @@
 -export([advance/1, create/0, direction/1, left/1, place/3, position/1, right/1]).
 
 
+-record(state, {server, position = {undefined, undefined}, direction=undefined}).
+
+
 advance(_Robot) -> undefined.
 
 create() -> undefined.
