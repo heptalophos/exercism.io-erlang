bd6a87ea1fab94605edd2f24abab83e8a55a3166
Zero Delta <zerodeltahedge@gmail.com>
2019-09-30 21:05:42 +0300

    run-length-encoding solution 1st iteration

--- run-length-encoding/src/run_length_encoding.erl
+++ run-length-encoding/src/run_length_encoding.erl
@@ -2,7 +2,38 @@
 
 -export([decode/1, encode/1]).
 
+decode(String) ->
+    case String of
+        [] -> "";
+        _  -> 
+            [L|R] = lists:dropwhile(fun(C) -> C >= $0 andalso C =< $9 end, String),
+            Decoded = decode_group(L, 
+                                   lists:takewhile(
+                                       fun(C) -> C >= $0 andalso C =< $9 end, 
+                                       String)),
+            Decoded ++ decode(R)
+    end.
 
-decode(_String) -> undefined.
+encode(String) ->
+    case String of
+        [] -> "";
+        _  ->
+            [L | R] = String,
+            Encoded = encode_group(L, 
+                                   length([L | lists:takewhile(
+                                                    fun(C) -> C == L end, 
+                                                    R)])),
+            Encoded ++ encode(lists:dropwhile(fun(C) -> C == L end, R))
+    end.
 
-encode(_String) -> undefined.
+decode_group(L, N) -> 
+    if 
+        N =/= [] -> lists:duplicate(list_to_integer(N), L);
+        true -> [L]
+    end.
+
+encode_group(L, N) -> 
+    if 
+        N =/= 1 -> integer_to_list(N) ++ [L];
+        true -> [L]
+    end.
