16995c985f6d1e05fd11ae70ccde411b87c25965
Zero Delta <zerodeltahedge@gmail.com>
2019-10-10 19:18:55 +0300

    word-count solution (1st iteration)

--- word-count/src/word_count.erl
+++ word-count/src/word_count.erl
@@ -3,4 +3,24 @@
 -export([count_words/1]).
 
 
-count_words(_Sentence) -> undefined.
+-spec count_words(string()) -> #{string() => pos_integer()}.
+count_words(Sentence) -> 
+    lists:foldl(fun (Word, Count) -> 
+                    maps:update_with(Word, 
+                                     fun(X) -> X + 1 end,
+                                     1, 
+                                     Count) 
+                end,
+                maps:new(),
+                words(Sentence)).
+
+
+% Auxiliary
+
+-spec words(string()) -> [string()].
+words(Sentence) ->
+    [string:to_lower(Word) || 
+     Word <- re:split(Sentence, 
+                      "\\W'|'\\W|[^\\w']+", 
+                      [{return, list}]), 
+     Word =/= []].
