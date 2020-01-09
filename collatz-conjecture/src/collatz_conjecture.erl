732d41b90e3dbcd5de4d20de127e986fdd54112f
Zero Delta <zerodeltahedge@gmail.com>
2020-01-09 23:26:56 +0200

    Refactor collatz_conjecture.erl

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -2,10 +2,11 @@
 
 -export([steps/1]).
 
--spec steps(N::integer()) -> 
-    Steps::non_neg_integer() | {error, string()}.
-steps(1) -> 0;
-steps(N) when N =< 0 orelse not is_integer(N) -> 
-    {error, "Only positive numbers are allowed"};
-steps(N) when N rem 2 =:= 0 -> steps(N div 2) + 1;
-steps(N) -> steps(3 * N + 1) + 1.
+-spec steps(integer()) -> non_neg_integer() | error.
+steps(N) when is_integer(N) andalso N > 0 -> steps(N, 0);
+steps(_) -> error(badarg).
+
+-spec steps(N::integer(), Step::non_neg_integer()) -> non_neg_integer().
+steps(1, Step) -> Step;
+steps(N, Step) when N rem 2 =:= 0 -> steps(N div 2, Step + 1);
+steps(N, Step) -> steps(3 * N + 1, Step + 1).
