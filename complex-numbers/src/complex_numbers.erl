c2388554ee2e884f737fd3a62b463905ecf7a631
Zero Delta <zerodeltahedge@gmail.com>
2020-06-23 23:08:23 +0300

    complex-numbers 1st iteration

--- complex-numbers/src/complex_numbers.erl
+++ complex-numbers/src/complex_numbers.erl
@@ -1,27 +1,48 @@
 -module(complex_numbers).
 
--export([abs/1, add/2, conjugate/1, divide/2, equal/2, exp/1, imaginary/1, mul/2, new/2,
-	 real/1, sub/2]).
+-export([abs/1, 
+		 add/2, 
+		 conjugate/1, 
+		 divide/2, 
+		 equal/2, 
+		 exp/1, 
+		 imaginary/1, 
+		 mul/2, 
+		 new/2, 
+		 real/1, 
+		 sub/2]).
 
 
+-spec abs(complex()) -> number().
 abs(_Z) -> undefined.
 
+
+-spec add(complex(), complex()) -> complex().
 add(_Z1, _Z2) -> undefined.
 
+-spec congugate(complex()) -> complex().
 conjugate(_Z) -> undefined.
 
+-spec divide(complex(), complex()) -> complex().
 divide(_Z1, _Z2) -> undefined.
 
+-spec equal(complex(), complex()) -> boolean().
 equal(_Z1, _Z2) -> undefined.
 
+-spec complex() -> ().
 exp(_Z) -> undefined.
 
+-spec abs() -> ().
 imaginary(_Z) -> undefined.
 
+-spec abs() -> ().
 mul(_Z1, _Z2) -> undefined.
 
+-spec abs() -> ().
 new(_R, _I) -> undefined.
 
+-spec abs() -> ().
 real(_Z) -> undefined.
 
+-spec abs() -> ().
 sub(_Z1, _Z2) -> undefined.
