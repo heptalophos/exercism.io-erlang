35af6e02ea5cf3a72339569e3722473528f93c5d
Zero Delta <zerodeltahedge@gmail.com>
2020-06-23 23:15:40 +0300

    Reformat accumulate

--- accumulate/src/accumulate.erl
+++ accumulate/src/accumulate.erl
@@ -2,16 +2,24 @@
 
 -export([accumulate/2]).
 
--spec accumulate(fun((A) -> B), list(A)) -> list(B).
-accumulate(Fn, Ls) -> accumulate(Fn, Ls, []).
+-spec accumulate(fun((A) -> B), list(A)) -> 
+                                    list(B).
+accumulate(Fn, Ls) -> 
+    accumulate(Fn, Ls, []).
 
--spec accumulate(fun((A) -> B), list(A), list(B)) -> list(B).
-accumulate(_, [], Acc)     -> reverse(Acc);
-accumulate(Fn, [H|T], Acc) -> accumulate(Fn, T, [Fn(H)|Acc]).
+-spec accumulate(fun((A) -> B), list(A), list(B)) -> 
+                                            list(B).
+accumulate(_, [], Acc) -> 
+    reverse(Acc);
+accumulate(Fn, [H|T], Acc) -> 
+    accumulate(Fn, T, [Fn(H)|Acc]).
 
 -spec reverse(list(A)) -> list(A).
-reverse(Ls) -> reverse(Ls, []).
+reverse(Ls) -> 
+    reverse(Ls, []).
 
 -spec reverse(list(A), list(A)) -> list(A).
-reverse([], Ls)    -> Ls;
-reverse([H|T], Ls) -> reverse(T, [H|Ls]).
+reverse([], Ls) -> 
+    Ls;
+reverse([H|T], Ls) -> 
+    reverse(T, [H|Ls]).
