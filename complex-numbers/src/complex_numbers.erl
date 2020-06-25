cd84893b21913fdb871f5cc5ab141daa96fda0f1
Zero Delta <zerodeltahedge@gmail.com>
2020-06-25 23:33:52 +0300

    complex-numbers 1st iteration

--- complex-numbers/src/complex_numbers.erl
+++ complex-numbers/src/complex_numbers.erl
@@ -13,49 +13,79 @@
 		 sub/2]).
 
 -record(complex, {real = 0, imag = 0}).
-
--export_type([complex/0]).
+-type complex() :: #complex{}.
 
 
 -spec abs(complex()) -> number().
 abs(Z) -> 
-	{R, I} = Z,
+	#complex{real = R, imag = I} = Z,
 	trunc(math:sqrt(R * R + I * I)).
 
 
 -spec add(complex(), complex()) -> complex().
 add(Z1, Z2) -> 
-	{R1, I1} = Z1,
-	{R2, I2} = Z2,
-	Z = {R1 + R2, I1 + I2},
-	Z.
+	#complex{real = R1, imag = I1} = Z1,
+	#complex{real = R2, imag = I2} = Z2,
+	Sum = #complex{real = R1 + R2, 
+				 imag = I1 + I2},
+	Sum.
 
 -spec conjugate(complex()) -> complex().
 conjugate(Z) -> 
-	{R, I} = Z,
-	Conj = {R, -I},
+	#complex{real = R, imag = I} = Z,
+	Conj = #complex{real = R, imag = -1 * I},
 	Conj.
 
 -spec divide(complex(), complex()) -> complex().
-divide(Z1, Z2) -> undefined.
+divide(Z1, Z2) -> 
+	#complex{real = R1, imag = I1} = Z1,
+	#complex{real = R2, imag = I2} = Z2,
+	Den = R2 * R2 + I2 * I2,
+	Div = #complex{real = (R1 * R2 + I1 * I2) / Den, 
+		   		imag = (I1 * R2 - I2 * R1) / Den},
+	Div.
 
 -spec equal(complex(), complex()) -> boolean().
-equal(Z1, Z2) -> undefined.
+equal(Z1, Z2) -> 
+	#complex{real = R1, imag = I1} = Z1,
+	#complex{real = R2, imag = I2} = Z2,
+	D = 0.005,
+	erlang:abs(R1 - R2) < D andalso 
+	erlang:abs(I1 - I2) < D.
 
--spec complex() -> ().
-exp(_Z) -> undefined.
+-spec exp(complex()) -> complex().
+exp(Z) -> 
+	#complex{real = R, imag = I} = Z,
+	Exp = #complex{real = math:exp(R) * math:cos(I), 
+		           imag = math:exp(R) * math:sin(I)},
+	Exp.
 
--spec abs() -> ().
-imaginary(_Z) -> undefined.
+-spec imaginary(complex()) -> number().
+imaginary(Z) -> 
+	#complex{real = _, imag = I} = Z,
+	I.
 
--spec abs() -> ().
-mul(_Z1, _Z2) -> undefined.
+-spec mul(complex(), complex()) -> complex().
+mul(Z1, Z2) -> 
+	#complex{real = R1, imag = I1} = Z1,
+	#complex{real = R2, imag = I2} = Z2,
+	Mul = #complex{real = R1 * R2 - I1 * I2, 
+				   imag = R2 * I1 + R1 * I2},
+	Mul.
 
--spec abs() -> ().
-new(_R, _I) -> undefined.
+-spec new(number(), number()) -> complex().
+new(R, I) -> 
+	#complex{real = R, imag = I}.
 
--spec abs() -> ().
-real(_Z) -> undefined.
+-spec real(complex()) -> number().
+real(Z) -> 
+	#complex{real = R, imag = _} = Z,
+	R.
 
--spec abs() -> ().
-sub(_Z1, _Z2) -> undefined.
+-spec sub(complex(), complex()) -> complex().
+sub(Z1, Z2) ->
+	#complex{real = R1, imag = I1} = Z1,
+	#complex{real = R2, imag = I2} = Z2, 
+	Diff = #complex{real = R1 - R2, 
+				    imag = I1 - I2},
+	Diff.
