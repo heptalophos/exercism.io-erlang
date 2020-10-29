0ce975f91e9ffffd0999dc99f86d9547f37802f4
Zero Delta <zerodeltahedge@gmail.com>
2020-10-29 13:20:51 +0200

    Update hamming.erl

--- hamming/src/hamming.erl
+++ hamming/src/hamming.erl
@@ -3,13 +3,13 @@
 -export([distance/2]).
 
 -spec distance(string(),string()) -> 
-               non_neg_integer() | {error, string()}.
+      non_neg_integer() 
+      | {error, string()}.
 distance(Strand1, Strand2) -> 
     case (length(Strand1) =/= length(Strand2)) of 
         true -> 
-            {error,
-             "left and right strands must" ++
-             " be of equal length"};
+            {error, "left and right strands must" 
+                    ++ " be of equal length"};
         false ->
             lists:sum([1 || {N1, N2} <- 
                             lists:zip(Strand1, Strand2), 
