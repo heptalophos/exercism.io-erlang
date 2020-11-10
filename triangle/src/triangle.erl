8347bee7362f5899ddcd2dc0b2c712e5cd7f9837
Zero Delta <zerodeltahedge@gmail.com>
2020-11-10 18:21:59 +0200

    Update triangle.erl

--- triangle/src/triangle.erl
+++ triangle/src/triangle.erl
@@ -3,42 +3,34 @@
 -export([kind/3]).
 
 -type triangle_form() :: equilateral 
-                         | isosceles 
-                         | scalene.
+                        | isosceles 
+                        | scalene.
 
--spec triangle_inequality([integer()]) -> 
-                    atom().
-triangle_inequality([A, B, C]) 
-        when (C < (A + B)) -> 
-        valid_triangle;
+-spec triangle_inequality([integer()]) -> atom().
+triangle_inequality([A, B, C]) when (C < (A + B)) -> 
+                        valid_triangle;
 triangle_inequality([_, _, _]) -> 
-        violated_triangle_inequality.
+                        violated_triangle_inequality.
 
--spec is_valid(number(), number(), number()) ->     
-                    atom().
-is_valid(A, B, C) when ((A =< 0) 
-                        or (B =< 0) 
-                        or (C =< 0)) -> 
-                not_all_sides_positive;
+-spec is_valid(number(), number(), number()) -> atom().
+is_valid(A, B, C) when ((A =< 0) or (B =< 0) or (C =< 0)) -> 
+                        not_all_sides_positive;
 is_valid(A, B, C) -> 
                 triangle_inequality(lists:sort([A, B, C])).
 
 -spec triangle(integer()) -> triangle_form().
-triangle(1) -> 
-    equilateral;
-triangle(2) -> 
-    isosceles;
-triangle(3) -> 
-    scalene.
+triangle(1) -> equilateral;
+triangle(2) -> isosceles;
+triangle(3) -> scalene.
 
 -spec kind(number(), number(), number()) -> 
-            triangle_form() | {error, string()}.
+                    triangle_form() | {error, string()}.
 kind(A, B, C) -> 
     case is_valid(A, B, C) of
         not_all_sides_positive -> 
-                {error, "all side lengths must be positive"};
+            {error, "all side lengths must be positive"};
         violated_triangle_inequality -> 
-                {error, "side lengths violate triangle inequality"};
+            {error, "side lengths violate triangle inequality"};
         valid_triangle -> 
-                triangle(sets:size(sets:from_list([A, B, C])))
+            triangle(sets:size(sets:from_list([A, B, C])))
     end.
