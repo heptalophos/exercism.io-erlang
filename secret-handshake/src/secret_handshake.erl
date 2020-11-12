63e7a29e9a6f5c1f41429e052799ace0c3dc0671
Zero Delta <zerodeltahedge@gmail.com>
2020-11-12 20:01:57 +0200

    Update secret_handshake.erl

--- secret-handshake/src/secret_handshake.erl
+++ secret-handshake/src/secret_handshake.erl
@@ -6,24 +6,22 @@
 commands(Number) -> 
     Commands = lists:filter(
         fun(X) -> X =/= nil end,
-        [ 
-            case Number band 16#01 =/= 0 of 
+        [ case Number band 16#01 =/= 0 of 
                 true -> "wink";
                 _    ->  nil 
-            end,
-            case Number band 16#02 =/= 0 of 
+          end,
+          case Number band 16#02 =/= 0 of 
                 true -> "double blink";    
                 _    ->  nil 
-            end,
-            case Number band 16#04 =/= 0 of 
+          end,
+          case Number band 16#04 =/= 0 of 
                 true -> "close your eyes"; 
                 _    ->  nil 
-            end,
-            case Number band 16#08 =/= 0 of 
+          end,
+          case Number band 16#08 =/= 0 of 
                 true -> "jump";
                 _    ->  nil 
-            end 
-        ]
+          end ]
     ),
     case Number band 16#10 =/= 0 of 
         true -> lists:reverse(Commands);
