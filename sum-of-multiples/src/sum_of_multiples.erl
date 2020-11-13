deded569fa8164b4d43904d1d821ca8013c3ba04
Zero Delta <zerodeltahedge@gmail.com>
2020-11-13 02:10:15 +0200

    Update sum_of_multiples.erl

--- sum-of-multiples/src/sum_of_multiples.erl
+++ sum-of-multiples/src/sum_of_multiples.erl
@@ -2,18 +2,21 @@
 
 -export([sum/2]).
 
--spec sum(Factors::[non_neg_integer()], Limit::pos_integer()) -> 
-          non_neg_integer().
+-spec sum(Factors::[non_neg_integer()], 
+          Limit::pos_integer()) -> 
+                      non_neg_integer().
 
 sum(Factors, Limit) -> 
-    lists:foldl(fun (M, Sum) -> Sum + M end, 
+    lists:foldl(fun (M, Sum) -> 
+                Sum + M end, 
                 0, 
                 [X || X <- lists:seq(0, Limit - 1),
-                 lists:any(fun (M) -> if
-                                        X rem M =:= 0 -> true;
-                                        X =:= 0 -> true;
-                                        true -> false 
-                                      end
-                            end,
+                 lists:any(fun (M) -> 
+                                if
+                                  X rem M =:= 0 -> true;
+                                  X =:= 0 -> true;
+                                  true -> false 
+                                end
+                           end,
                  Factors)]
                ).
