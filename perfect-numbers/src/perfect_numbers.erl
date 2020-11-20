21a90ccf4058da2669d2b3d32d4e8fb6921839d4
Zero Delta <zerodeltahedge@gmail.com>
2020-11-20 04:21:02 +0200

    Update perfect_numbers.erl

--- perfect-numbers/src/perfect_numbers.erl
+++ perfect-numbers/src/perfect_numbers.erl
@@ -2,17 +2,23 @@
 
 -export([classify/1]).
 
--type nicomachusclass() :: perfect | abundant | deficient.
+-type nicomachusclass() :: perfect 
+                           | abundant 
+                           | deficient.
 
--spec classify(pos_integer()) -> nicomachusclass() | error.
+-spec classify(pos_integer()) -> 
+                    nicomachusclass() | error.
+
 classify(Number) when Number > 0 ->
-    Factors = [Factor || Factor <- lists:seq(1, Number div 2), 
-                                   Number rem Factor == 0],
+    Factors = [Factor || Factor <- 
+                         lists:seq(1, Number div 2), 
+                         Number rem Factor == 0],
     Aliquot = lists:sum(Factors),
     if 
         Aliquot >  Number -> abundant;
         Aliquot == Number -> perfect;
         Aliquot <  Number -> deficient
     end;
+
 classify(_) -> 
     error("Not a natural number").
