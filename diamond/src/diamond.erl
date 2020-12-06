b44123cf921d7c5832412c87e3186f86d2d264e0
Zero Delta <zerodeltahedge@gmail.com>
2020-12-06 18:58:43 +0200

    Reformat diamond

--- diamond/src/diamond.erl
+++ diamond/src/diamond.erl
@@ -3,18 +3,15 @@
 -export([rows/1]).
 
 
--spec spaces(non_neg_integer()) -> 
-        [$ ,...].
-spaces (0) -> 
-    "";
+-spec spaces(non_neg_integer()) -> [$ ,...].
+spaces (0) -> "";
 spaces (N) -> 
     [" " || _ <- lists:seq(1, N) ].
 
 
 -spec row([$A..$Z], integer(), integer()) -> 
-        [[$A..$Z | $ ],...].
-row(Letter, Outside, Middle) 
-    when Middle > 0 -> 
+                               [[$A..$Z | $ ],...].
+row(Letter, Outside, Middle) when Middle > 0 -> 
     lists:flatten([spaces(Outside), 
                    Letter, 
                    spaces(Middle), 
@@ -26,13 +23,9 @@ row(Letter, Outside, _) ->
                    spaces(Outside)]).
 
 
--spec diamond([$A..$Z], 
-              integer(), 
-              integer(), 
-              [[$A..$Z | $ ],...]) -> 
-        [[$A..$Z | $ ],...].
-diamond(Letter, Outside, Middle, Acc) 
-    when Middle < 1 ->
+-spec diamond([$A..$Z],integer(),integer(),[[$A..$Z | $ ],...]) -> 
+                                           [[$A..$Z | $ ],...].
+diamond(Letter, Outside, Middle, Acc) when Middle < 1 ->
         Upper = 
             [row(Letter, Outside, Middle) | Acc],
         [_|Lower] = 
@@ -45,7 +38,6 @@ diamond(Letter, Outside, Middle, Acc) ->
             [row(Letter, Outside, Middle) | Acc]).
 
 
--spec rows([$A..$Z]) -> 
-        [[$A..$Z | $ ], ...].
+-spec rows([$A..$Z]) -> [[$A..$Z | $ ], ...].
 rows([Letter]) -> 
     diamond(Letter, 0, 2 * (Letter - $A) - 1, []).
