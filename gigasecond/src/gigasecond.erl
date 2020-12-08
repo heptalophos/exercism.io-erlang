19545580c9fda09d9937638275d042ebdbfddb35
Zero Delta <zerodeltahedge@gmail.com>
2020-12-08 19:46:21 +0200

    Update gigasecond.erl

--- gigasecond/src/gigasecond.erl
+++ gigasecond/src/gigasecond.erl
@@ -5,11 +5,12 @@
 -define(GIGASEC, 1000000000).
 
 -spec from(calendar:date() | calendar:datetime()) -> 
-    calendar:datetime().
+                                calendar:datetime().
 
 from(Date = { _Y, _M, _D }) -> 
     from({ Date, { 0, 0, 0 }});
 
 from({ Date = { _Y, _M, _D }, Time = { _H, _Mi, _S }}) -> 
-    Seconds = calendar:datetime_to_gregorian_seconds({ Date, Time }),
+    Seconds = 
+        calendar:datetime_to_gregorian_seconds({ Date, Time }),
     calendar:gregorian_seconds_to_datetime(Seconds + ?GIGASEC).
