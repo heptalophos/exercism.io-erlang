72a3f9724b32efab6ac34c02d51db127233256a0
Zero Delta <zerodeltahedge@gmail.com>
2021-01-07 23:32:29 +0200

    Update protein_translation.erl

--- protein-translation/src/protein_translation.erl
+++ protein-translation/src/protein_translation.erl
@@ -14,34 +14,55 @@
 
 
 -spec proteins(string()) -> polypeptide().
-proteins(Strand) -> lists:reverse(proteins(Strand, [])).
+proteins(Strand) -> 
+    lists:reverse(proteins(Strand, [])).
 
 
 % auxiliary
 
 -spec proteins([char()], [char()]) -> polypeptide().
-proteins([], Polypeptide) -> Polypeptide;
+proteins([], Polypeptide) -> 
+    Polypeptide;
 proteins([X, Y, Z | Rest], Polypeptide) -> 
-    case codon2amino([X, Y, Z]) of
-        stop -> proteins([], Polypeptide);
-        AA   -> proteins(Rest, [AA | Polypeptide])
-    end.
+        case codon2amino([X, Y, Z]) of
+            stop -> 
+                proteins([], Polypeptide);
+            AA -> 
+                proteins(Rest, [AA | Polypeptide])
+        end.
 
 -spec codon2amino(string()) -> aminoacid() | control_seq().
-codon2amino("AUG") -> methionine;
-codon2amino("UUU") -> phenylalanine;
-codon2amino("UUC") -> phenylalanine;
-codon2amino("UUA") -> leucine;
-codon2amino("UUG") -> leucine;
-codon2amino("UCU") -> serine;
-codon2amino("UCC") -> serine;
-codon2amino("UCA") -> serine;
-codon2amino("UCG") -> serine;
-codon2amino("UAU") -> tyrosine;
-codon2amino("UAC") -> tyrosine;
-codon2amino("UGU") -> cysteine;
-codon2amino("UGC") -> cysteine;
-codon2amino("UGG") -> tryptophan;
-codon2amino("UAA") -> stop;
-codon2amino("UAG") -> stop;
-codon2amino("UGA") -> stop.
+codon2amino("AUG") -> 
+    methionine;
+codon2amino("UUU") -> 
+    phenylalanine;
+codon2amino("UUC") -> 
+    phenylalanine;
+codon2amino("UUA") -> 
+    leucine;
+codon2amino("UUG") -> 
+    leucine;
+codon2amino("UCU") -> 
+    serine;
+codon2amino("UCC") -> 
+    serine;
+codon2amino("UCA") -> 
+    serine;
+codon2amino("UCG") -> 
+    serine;
+codon2amino("UAU") -> 
+    tyrosine;
+codon2amino("UAC") -> 
+    tyrosine;
+codon2amino("UGU") -> 
+    cysteine;
+codon2amino("UGC") -> 
+    cysteine;
+codon2amino("UGG") -> 
+    tryptophan;
+codon2amino("UAA") -> 
+    stop;
+codon2amino("UAG") -> 
+    stop;
+codon2amino("UGA") -> 
+    stop.
