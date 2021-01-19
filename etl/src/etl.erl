f36b4d01602b8f8cd1e7634dfa05afe6d7798c09
Invigo <invigo.devel@gmail.com>
2021-01-20 01:56:49 +0200

    Revert "Update etl.erl"
    This reverts commit cdf4f4e0544bb6a96dd1eeb10a5227f9d7094333.

--- etl/src/etl.erl
+++ etl/src/etl.erl
@@ -1,20 +1,6 @@
 -module(etl).
--author(heptalophos).
 
 -export([transform/1]).
 
--type legacy() :: [{pos_integer(), [string()]}].
--type migrated() :: [{string(), pos_integer()}].
 
--spec transform(legacy()) -> migrated().
-transform(Legacy) -> 
-    Transformation =
-        fun({Letter, Points}, Transformed) ->
-            Reformed = 
-                lists:map(fun (L) -> 
-                    {string:to_lower(L), Letter} 
-                          end, 
-                Points),
-            lists:merge(Transformed, Reformed)
-        end,
-    lists:foldl(Transformation, [], Legacy).
+transform(_Old) -> undefined.
