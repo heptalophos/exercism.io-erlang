7bbd07eb51ee4f1f8a90c58a8c035a4d3f9c46b9
Zero Delta <zerodeltahedge@gmail.com>
2021-01-20 02:00:50 +0200

    Update etl.erl

--- etl/src/etl.erl
+++ etl/src/etl.erl
@@ -1,6 +1,20 @@
 -module(etl).
+-author(heptalophos).
 
 -export([transform/1]).
 
+-type legacy() :: [{pos_integer(), [string()]}].
+-type migrated() :: [{string(), pos_integer()}].
 
-transform(_Old) -> undefined.
+-spec transform(legacy()) -> migrated().
+transform(Legacy) -> 
+    Transformation =
+        fun({Letter, Points}, Transformed) ->
+            Reformed = 
+                lists:map(fun (L) -> 
+                    {string:to_lower(L), Letter} 
+                          end, 
+                Points),
+            lists:merge(Transformed, Reformed)
+        end,
+    lists:foldl(Transformation, [], Legacy).
