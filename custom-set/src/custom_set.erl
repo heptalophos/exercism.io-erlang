1d80810f3a8f3a152bce456801f04774a97b1904
Zero Delta <zerodeltahedge@gmail.com>
2021-02-11 22:53:42 +0200

    Update custom_set.erl

--- custom-set/src/custom_set.erl
+++ custom-set/src/custom_set.erl
@@ -12,32 +12,43 @@
 	 	 union/2
 		]).
 
--spec add(any(), set()) -> set().
-add(_Elem, _Set) -> undefined.
+-spec add(any(), list()) -> list().
+add(Elem, Set) -> 
+	maps:put(Elem, true, Set).
 
--spec contains(any(), set()) -> boolean().
-contains(_Elem, _Set) -> undefined.
+-spec contains(any(), list()) -> boolean().
+contains(Elem, Set) -> 
+	maps:get(Elem, Set, false).
 
--spec difference(set(), set()) -> set().
-difference(_Set1, _Set2) -> undefined.
+-spec difference(list(), list()) -> list().
+difference(Set1, Set2) -> 
+	maps:without(maps:keys(Set2), Set1).
 
--spec disjoint(set(), set()) -> boolean().
-disjoint(_Set1, _Set2) -> undefined.
+-spec disjoint(list(), list()) -> boolean().
+disjoint(Set1, Set2) -> 
+	maps:size(intersection(Set1, Set2)) =:= 0.
 
--spec empty(set()) -> boolean.
-empty(_Set) -> undefined.
+-spec empty(list()) -> boolean.
+empty(Set) -> 
+	maps:size(Set) =:= 0.
 
--spec equal(set(), set()) -> boolean().
-equal(_Set1, _Set2) -> undefined.
+-spec equal(list(), list()) -> boolean().
+equal(Set1, Set2) -> 
+	Set1 =:= Set2.
 
--spec from_list(list()) -> set().
-from_list(_List) -> undefined.
+-spec from_list(list()) -> list().
+from_list(List) -> 
+	maps:from_list([{Elem, true} || Elem <- List]).
 
--spec intersection(set(), set()) -> set().
-intersection(_Set1, _Set2) -> undefined.
+-spec intersection(list(), list()) -> list().
+intersection(Set1, Set2) -> 
+	from_list([E || E <- maps:keys(Set1), contains(E, Set2)]).
+	% [Elem || Elem <- Set1, contains(Elem, Set2)].
 
--spec subset(set(), set()) -> boolean().
-subset(_Set1, _Set2) -> undefined.
+-spec subset(list(), list()) -> boolean().
+subset(Set1, Set2) -> 
+	lists:all(fun(E) -> contains(E, Set2) end, maps:keys(Set1)).
 
--spec (union(set(), set()) -> set().)
-union(_Set1, _Set2) -> undefined.
+-spec union(list(), list()) -> list().
+union(Set1, Set2) -> 
+	maps:merge(Set1, Set2).
