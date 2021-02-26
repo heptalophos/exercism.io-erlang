f2b3a8e0543fd950086cfa09387ad97f234e78c6
Zero Delta <zerodeltahedge@gmail.com>
2021-02-26 23:25:38 +0200

    Formulate series solution

--- series/src/series.erl
+++ series/src/series.erl
@@ -2,5 +2,22 @@
 
 -export([slices/2]).
 
+-spec slices(pos_integer(), [any()]) -> [any()] | {error, string()}.
+slices(SliceLength, Series) ->
+    if 
+        SliceLength =< 0 ->
+            error("vanishing or illegal slices");
+        SliceLength > length(Series) ->
+            error("slice can't be longer than series");
+        true ->
+            slices(SliceLength, Series, [])
+    end.   
 
-slices(_SliceLength, _Series) -> undefined.
+
+% Auxiliary
+
+-spec slices(pos_integer(), [any()], [any()]) -> [any()].
+slices(SLength, Series, SubSeries) when length(Series) < SLength ->
+    lists:reverse(SubSeries);
+slices(SLength, Series = [_|Rest], SubSeries) -> 
+    slices(SLength, Rest, [lists:sublist(Series, SLength) | SubSeries]).
