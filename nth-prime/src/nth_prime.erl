fdbd1f9506b3dd91b2cfa41b0174061a4698c7c7
Zero Delta <zerodeltahedge@gmail.com>
2021-02-26 23:21:54 +0200

    Refactor nth_prime.erl

--- nth-prime/src/nth_prime.erl
+++ nth-prime/src/nth_prime.erl
@@ -2,22 +2,34 @@
 
 -export([prime/1]).
 
+-spec prime(pos_integer()) -> pos_integer()|{error, string()}.
+prime(N) -> case N of
+                0 -> error("badarg 0");
+                1 -> 2;
+                _ -> prime(N, 1)
+            end.
 
-prime(_N) -> undefined.
 
-% Aux
+% Auxiliary Functions
 
+-spec prime(pos_integer(), pos_integer()) -> pos_integer().
+prime(N, C) -> 
+    Truth = fun (X) -> 
+                case X of 
+                    true  -> 1; 
+                    false -> 0
+                end 
+            end,
+    case N of 
+        1 -> C;
+        _ -> prime(N - Truth(is_prime(C + 2)), C + 2)
+    end.
+    
 -spec is_prime(pos_integer()) -> boolean().
-is_prime(1) -> false;
-is_prime(2) -> true;
 is_prime(N) -> 
-    lists:all(fun(X) -> 
-                N rem X =/=0 end, 
-              lists:seq(2, ceil(math:sqrt(N)))).
-
--spec truth(boolean()) -> 1 | 0.
-truth(Tval) ->
-    case Tval of
-        true -> 1;
-        false -> 0
+    case N of 
+        1 -> false;
+        2 -> true;
+        _ -> lists:all(fun(X) -> N rem X =/= 0 end, 
+                       lists:seq(2, ceil(math:sqrt(N))))
     end.
