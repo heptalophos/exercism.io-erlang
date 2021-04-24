65014bd16aa80e37ecaf95c0b3f695f26b492135
Zero Delta <zerodeltahedge@gmail.com>
2021-04-24 22:56:43 +0300

    Update simple_linked_list.erl

--- simple-linked-list/src/simple_linked_list.erl
+++ simple-linked-list/src/simple_linked_list.erl
@@ -1,15 +1,44 @@
 -module(simple_linked_list).
 
--export([cons/2, count/1, empty/0, from_native_list/1,
-     head/1, reverse/1, tail/1, to_native_list/1]).
+-export([cons/2, 
+         count/1, 
+         empty/0,
+         head/1, 
+         tail/1,
+         reverse/1, 
+         from_native_list/1, 
+         to_native_list/1]).
 
-empty() -> undefined.
+-record(node, {payload :: any(), 
+               next    :: node()}).
 
-cons(_Elt, _List) -> undefined.
+-record(sllist, {head  :: node(), 
+                 tail  :: [node()], 
+                 count :: pos_integer()}).
 
-head(_List) -> undefined.
+-record(sllist, {payload = none :: any(), next = [] :: [any()]}).
 
-tail(_List) -> undefined.
+-type node() :: #node{}.
+
+-type sl_list() :: #sllist{}.
+
+-spec empty() -> sl_list().
+empty() -> 
+    #sllist{payload = none, next = []}.
+
+-spec cons(any(), sl_list()) -> sl_list().
+cons(Elt, List) -> 
+    Count = 1 + #sllist.count,
+    Next = List#node.next,
+    #sllist{payload = Elt, next = List}.
+
+head(List) -> 
+    #sllist{payload = Head, next = _} = List,
+    Head.
+
+tail(List) -> 
+    #sllist{payload = _, next = Tail} = List,
+    Tail.
 
 reverse(_List) -> undefined.
 
