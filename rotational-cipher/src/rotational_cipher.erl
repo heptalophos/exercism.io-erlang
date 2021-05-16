6bbaabefba5471b74179420e0ccac551195809a1
Zero Delta <zerodeltahedge@gmail.com>
2021-05-16 23:23:47 +0300

    Update rotational_cipher.erl

--- rotational-cipher/src/rotational_cipher.erl
+++ rotational-cipher/src/rotational_cipher.erl
@@ -2,7 +2,11 @@
 
 -export([decrypt/2, encrypt/2]).
 
-
+-spec decrypt(string(), non_negative_integer() -> string().
 decrypt(_String, _Key) -> undefined.
 
+-spec encrypt(string(), non_negative_integer() -> string().
 encrypt(_String, _Key) -> undefined.
+
+%  Auxiliary
+
