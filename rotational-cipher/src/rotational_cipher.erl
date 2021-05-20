4cdcdcdf3910e144e51455aaafed0574563ddabe
Zero Delta <zerodeltahedge@gmail.com>
2021-05-20 17:57:44 +0300

    Refactor rotational_cipher.erl

--- rotational-cipher/src/rotational_cipher.erl
+++ rotational-cipher/src/rotational_cipher.erl
@@ -2,11 +2,31 @@
 
 -export([decrypt/2, encrypt/2]).
 
--spec decrypt(string(), non_negative_integer() -> string().
-decrypt(_String, _Key) -> undefined.
+-define (MAX_ROT, 26).
 
--spec encrypt(string(), non_negative_integer() -> string().
-encrypt(_String, _Key) -> undefined.
+
+-spec encrypt(string(), non_neg_integer()) -> string().
+encrypt(_String, _Key) when _String =:= [] -> [];
+encrypt(String, Key) ->
+    [H|T] = String, 
+    [rotate(H, Key) | encrypt(T, Key)].
+
+-spec decrypt(string(), non_neg_integer()) -> string().
+decrypt(String, Key) -> 
+    encrypt(String, -Key rem ?MAX_ROT + ?MAX_ROT).
 
 %  Auxiliary
 
+-spec rotate(char(), non_neg_integer()) -> char().
+rotate(C, Shift) ->
+    case C of 
+        X when X >= $a andalso X =< $z ->
+            rotate(C, Shift, $a);
+        X when X >= $A andalso X =< $Z ->
+            rotate(C, Shift, $A);
+        _ -> C
+    end.
+
+-spec rotate(char(), non_neg_integer(), char()) -> char().
+rotate(C, Shift, Offset) ->
+    (C - Offset + Shift) rem ?MAX_ROT + Offset.
