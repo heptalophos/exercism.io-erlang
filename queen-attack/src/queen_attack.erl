bda99afd084e0673c0d8a13b1fe30ee9287bd0bc
Zero Delta <zerodeltahedge@gmail.com>
2021-07-14 01:13:07 +0300

    Refactor queen_attack.erl

--- queen-attack/src/queen_attack.erl
+++ queen-attack/src/queen_attack.erl
@@ -3,31 +3,26 @@
 -export([can_attack/2]).
 
 -spec can_attack({0..7, 0..7}, {0..7, 0..7}) -> 
-                 boolean() | {error, atom()}.
+                 boolean() | error.
 
 can_attack(WhiteQueen, BlackQueen) -> 
     {WX, WY} = WhiteQueen,
     {BX, BY} = BlackQueen,
     OffBoard = fun(X) -> 
-        if X < 0 orelse X > 7 -> true; 
-        true -> false 
+        if 
+            X < 0 orelse X > 7 -> true;
+            true -> false 
         end 
     end,
     case lists:any(OffBoard, [WX, WY, BX, BY]) of  
-        true  -> %% any queen @ illegal rank or file
-            {error, invalid_position_offboard};
-        false -> %% both queens @ legal rank & file
+        true  -> 
+            error(invalid_position);
+        false -> 
             case ({abs(WX - BX), abs(WY - BY)}) of 
-                %% both queens @ same square
-                {0, 0} -> {error, invalid_position_same};
-                %% both queens @ same file
+                {0, 0} -> error(on_same_square);
                 {_, 0} -> true;
-                %% both queens @ same rank
                 {0, _} -> true;
-                %% queens @ same diagonal
-                {X, Y} when X / Y == 1 -> true; 
-                %% otherwise
-                {_, _} -> false
+                {X, Y} -> X / Y == 1 
             end
     end.
 
