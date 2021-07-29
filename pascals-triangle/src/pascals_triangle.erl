c84598db9185d687642e03d29c08cc52d0bf7f1c
Zero Delta <zerodeltahedge@gmail.com>
2021-07-29 22:14:58 +0300

    Working 1st iteration, pascals-triangle

--- pascals-triangle/src/pascals_triangle.erl
+++ pascals-triangle/src/pascals_triangle.erl
@@ -4,4 +4,21 @@
 
 
 -spec rows(pos_integer()) -> [[pos_integer()]].
-rows(_Count) -> undefined.
+rows(Count) -> 
+        lists:map(
+            fun(X) -> 
+                    lists:map(
+                        fun(Y) -> 
+                               binomial(X - 1, Y) 
+                        end, 
+                        lists:seq(0, X - 1)
+                    ) 
+            end,
+            lists:seq(1, Count)).
+
+
+% Auxiliary
+
+-spec binomial(pos_integer, pos_integer) -> pos_integer.
+binomial(_, 0) -> 1;
+binomial(N, K) -> (N + 1 - K) * binomial(N, K - 1) div K.
