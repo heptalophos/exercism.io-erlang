92bfcc94a5849cdfa0c0a5059e77e6bde5a0532a
Zero Delta <zerodeltahedge@gmail.com>
2021-08-31 23:01:08 +0300

    Refactor collatz_conjecture.erl

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -3,17 +3,23 @@
 -export([steps/1]).
 
 -spec steps(integer()) -> non_neg_integer() | error.
-steps(N) when is_integer(N) andalso N > 0 -> 
-                                    steps(N, 0);
-steps(_) -> 
-        error(badarg).
+steps(N) ->
+    case is_integer(N) andalso N > 0 of
+        true -> 
+            steps(N, 0);
+        _ -> 
+            error(badarg)
+    end.
 
 -spec steps(N::integer(), 
             Step::non_neg_integer()) -> 
                                 non_neg_integer().
-steps(1, Step) -> 
-                Step;
-steps(N, Step) when N rem 2 =:= 0 -> 
-                steps(N div 2, Step + 1);
 steps(N, Step) -> 
-                steps(3 * N + 1, Step + 1).
+    case N of 
+        1 -> Step;
+        _ -> 
+            case N rem 2 =:= 0 of 
+                true -> steps(N div 2, Step + 1);
+                _ -> steps(3 * N + 1, Step + 1)
+            end
+    end.
