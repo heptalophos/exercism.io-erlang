58e174c2e62d4f1f2c029f1414fafcee8b5f3254
Zero Delta <zerodeltahedge@gmail.com>
2022-01-12 15:21:59 +0200

    Refactor saddle-points

--- saddle-points/src/saddle_points.erl
+++ saddle-points/src/saddle_points.erl
@@ -5,13 +5,13 @@
 -spec saddle_points([[integer()]]) -> [{integer(), integer()}].
 saddle_points(Matrix) ->
 	Saddle_point = 
-		fun (R, C, M) ->
-			Row =  lists:nth(R, M),
-			Col = [lists:nth(C, Nth) || Nth <- M],
-			Val =  lists:nth(C, lists:nth(R, M)),
+		fun (R, C) ->
+			Row =  lists:nth(R, Matrix),
+			Col = [lists:nth(C, Nth) || Nth <- Matrix],
+			Val =  lists:nth(C, lists:nth(R, Matrix)),
 			Val =:= lists:max(Row) andalso 
 			Val =:= lists:min(Col)			  
 		end,
 	[{R - 1, C - 1} || R <- lists:seq(1, length(Matrix)), 
 					   C <- lists:seq(1, length(hd(Matrix))), 
-					   Saddle_point(R, C, Matrix)].
+					   Saddle_point(R, C)].
