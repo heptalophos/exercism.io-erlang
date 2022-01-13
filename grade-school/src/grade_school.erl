5d171f29f5376476c9d1115abf9fadeba36a5027
Zero Delta <zerodeltahedge@gmail.com>
2022-01-13 23:33:26 +0200

    Create grade_school.erl

---
+++ grade-school/src/grade_school.erl
@@ -0,0 +1,14 @@
+-module(grade_school).
+
+-author("heptalophos").
+
+-export([add/3, get/2, get/1, new/0]).
+
+
+add(_Name, _Grade, _School) -> undefined.
+
+get(_Grade, _School) -> undefined.
+
+get(_School) -> undefined.
+
+new() -> undefined.
