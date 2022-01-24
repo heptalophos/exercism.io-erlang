f1a9d8d2a989f34c744557fa803ff7659edc4514
Zero Delta <zerodeltahedge@gmail.com>
2022-01-24 22:43:16 +0200

    Update palindrome_products.erl

--- palindrome-products/src/palindrome_products.erl
+++ palindrome-products/src/palindrome_products.erl
@@ -1,8 +1,68 @@
 -module(palindrome_products).
 
--export([largest/2, smallest/2]).
+-export([largest/2, smallest/2, palindromes/2, palindromes/3]).
 
 
-largest(_Min, _Max) -> undefined.
+largest(Min, Max) -> 
+    case Min > Max of
+        true -> {error, invalid_range};
+        _ -> 
+            Factors = lists:seq(Min, Max),
+            palindromes(Factors, Factors, fun erlang:'>'/2)
+    end.
 
-smallest(_Min, _Max) -> undefined.
+smallest(Min, Max) -> 
+    case Min > Max of
+        true -> {error, invalid_range};
+        _ -> 
+            Factors = lists:seq(Min, Max),
+            palindromes(Factors, Factors, fun erlang:'<'/2)
+    end.
+
+% Auxiliary
+
+palindromes(Start, End) ->
+	case Start =:= End of
+		true -> [];
+		_ -> 
+			lists:flatten( palindromes( Start, Start, End ), 
+						   palindromes( Start + 1, End ) )
+	end.
+
+palindromes(F, S, End) -> 
+	if 
+		S =:= End ->
+			Product = F * End,
+			Palindrome = is_palindrome(Product),
+			case Palindrome of
+				true -> 
+					[ {{F, End}, Product} ];
+				_ -> 
+					[]
+			end;
+		true ->
+			Product = F * S,
+			Palindrome = is_palindrome(Product), 
+			case Palindrome of
+				true -> 
+					% [ { { F, S }, Product } | palindromes( F, S + 1, End ) ];
+					[ { {F, S}, Product } | palindromes( F, S + 1, End ) ];
+				_ -> 
+					palindromes(F, S + 1, End)
+			end
+	end.
+
+is_palindrome(P) -> 
+	S = integer_to_list(P), 
+	R = lists:reverse(S), 
+	if 
+		S == R -> true; 
+		true -> false 
+	end.
+
+% is_palindrome(A, B) ->
+% 	is_palindrome(A * B).
+
+
+
+
