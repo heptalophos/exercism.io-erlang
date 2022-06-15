8deff03cd561ffd1e0decbc8d15711314b08eee1
Zero Delta <zerodeltahedge@gmail.com>
2022-06-15 16:01:20 +0300

    Refactor grade_school.erl

--- grade-school/src/grade_school.erl
+++ grade-school/src/grade_school.erl
@@ -8,20 +8,22 @@
 -type student() :: unicode:chardata().
 -type school() :: [{grade(), student()}].
 
+-spec new() -> school().
+new() -> [].
+
 -spec add(student(), grade(), school()) -> school().
 add(Name, Grade, School) -> 
-    lists:sort([{G, S} 
-                || {G, S} <- [{Grade, Name} | School]]).
+    Names = lists:sort([S || {_, S} <- School]),
+    case lists:member(Name, Names) of 
+        true -> School; 
+        _    -> [{Grade, Name} | School]
+    end.
 
 -spec get(grade(), school()) -> [student()].
 get(Grade, School) -> 
-    [Student || 
-        {_, Student} <- lists:sort([{G, _S} || 
-            {G, _S} <- School, G == Grade])].
+    [Student || {_, Student} <- lists:sort([{G, _S} 
+             || {G, _S} <- School, G =:= Grade])].
 
 -spec get(school()) -> [student()].
 get(School) -> 
     lists:sort([Student || {_, Student} <- School]).
-
--spec new() -> school().
-new() -> [].
