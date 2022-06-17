01b31e803c1368233527c4d225cb6fade30f7cdf
Zero Delta <zerodeltahedge@gmail.com>
2022-06-17 03:01:49 +0300

    Reformat phone-number/src/phone_number.erl

--- phone-number/src/phone_number.erl
+++ phone-number/src/phone_number.erl
@@ -2,7 +2,6 @@
 
 -export([number/1, areacode/1, pretty_print/1]).
 
-
 -spec number(string()) -> [$0..$9].
 number(String) ->
   case [C || C <- String, is_digit(C)] of
@@ -22,7 +21,6 @@ pretty_print(String) ->
   [A1, A2, A3, X1, X2, X3, N1, N2, N3, N4] = number(String),
   {Area, XChange, Number} = {[A1, A2, A3], [X1, X2, X3], [N1, N2, N3, N4]},
   lists:flatten(io_lib:format("(~s) ~s-~s", [Area, XChange, Number])).
-
 
 % Auxiliary
 
