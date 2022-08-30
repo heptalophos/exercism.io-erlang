83e996798588e9ffca649cdae6843d4cb1ce8a94
Zero Delta <zerodeltahedge@gmail.com>
2022-08-31 01:42:06 +0300

    Update atbash_cipher.erl

--- atbash-cipher/src/atbash_cipher.erl
+++ atbash-cipher/src/atbash_cipher.erl
@@ -1,37 +1,33 @@
 -module(atbash_cipher).
--author(heptalophos).
 
 -export([decode/1, encode/1]).
 
+
 -spec decode(string()) -> string().
 decode(Phrase) -> 
-    lists:filtermap(fun revert/1, 
-                    string:lowercase(Phrase)).
+    lists:filtermap(fun atbash/1, string:lowercase(Phrase)).
 
 -spec encode(string()) -> string().
 encode(Phrase) -> 
-     chunks(5, decode(Phrase)).
+    chunks(5, decode(Phrase)).
 
-% auxiliary
 
--spec revert(char()) -> {true, char()} | false. 
-revert(C) -> 
+% Auxiliary
+
+-spec atbash(char()) -> {true, char()} | false. 
+atbash(C) -> 
     case (C >= $a) and ($z >= C) of
-        true  -> 
-            {true, ($z - C) + $a};
-        false -> 
-            case (C >= $0) and ($9 >= C) of
-                true -> {true, C};
-                false -> false
-            end
+        true  -> {true, ($z - C) + $a};
+        false -> case (C >= $0) and ($9 >= C) of
+                    true  -> {true, C};
+                    _     -> false
+                 end
     end.
 
 -spec chunks(pos_integer(), string()) -> string().
 chunks(N, S) ->
     case (N >= length(S)) of 
-        true -> 
-            S;
-        false -> 
-            {H, Rest} = lists:split(N, S),
-            H ++ " " ++ chunks(N, Rest)
+        true -> S;
+        _ -> {H, Rest} = lists:split(N, S), 
+              H ++ " " ++ chunks(N, Rest)
     end.
