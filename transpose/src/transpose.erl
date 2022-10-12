d0689f4238c041191cf64d83bf27c34d83084140
Zero Delta <zerodeltahedge@gmail.com>
2022-10-12 20:26:00 +0300

    transpose.erl - 1st passing iteration

--- transpose/src/transpose.erl
+++ transpose/src/transpose.erl
@@ -1,6 +1,36 @@
 -module(transpose).
 
--export([transpose/1]).
+-export([transpose/1, is_empty/1, pad/1]).
 
+-spec transpose([string()]) -> [string()].
+transpose(Lines) -> 
+    case is_empty(Lines) of
+        true -> [];
+        _    ->
+            Heads = [H || [H|_] <- Lines],
+            Tails = [T || [_|T] <- Lines],
+            case not lists:all(fun is_empty/1, Tails) of
+                true -> [Heads | transpose(pad(Tails))];
+                _    -> [Heads]
+            end
+    end.
 
-transpose(_Lines) -> undefined.
+% Auxiliary
+
+-spec is_empty(list()) -> boolean().
+is_empty(Ls) -> lists:all(fun(L) -> L =:= [] end, Ls).
+
+-spec pad(list()) -> list().
+pad(Ls) -> 
+    Reversed = lists:reverse(Ls),
+    Empty    = lists:takewhile(fun is_empty/1, Reversed), 
+    ToPad    = lists:dropwhile(fun is_empty/1, Reversed),
+    Padded   = lists:map(fun(L) -> 
+                            case is_empty(L) of 
+                                true -> " "; 
+                                _ -> L 
+                            end
+                         end, 
+                         ToPad
+                        ), 
+    lists:reverse(Empty ++ Padded).
