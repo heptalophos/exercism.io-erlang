c047ac8d6146bbd84c6b6a5d2dc7f73d08e291ae
Zero Delta <zerodeltahedge@gmail.com>
2022-10-31 03:08:19 +0200

    Update pythagorean_triplet.erl

--- pythagorean-triplet/src/pythagorean_triplet.erl
+++ pythagorean-triplet/src/pythagorean_triplet.erl
@@ -1,9 +1,6 @@
 -module(pythagorean_triplet).
-
 -export([triplets_with_sum/1]).
-
 -type triplet() :: {pos_integer(), pos_integer(), pos_integer()}.
-
 
 -spec triplets_with_sum(pos_integer()) -> list(triplet()).
 triplets_with_sum(Limit) -> triplets_with_sum(Limit, 1).
@@ -18,11 +15,11 @@ triplets_with_sum(Sum, Start) ->
 	        B = (square(Sum) - 2 * Sum * A) div (2 * Sum - 2 * A),
             R = (square(Sum) - 2 * Sum * A) rem (2 * Sum - 2 * A),
 	        C = Sum - A - B,
-            Pythagorean = R =:= 0 andalso B - A >= 0,
-            if Pythagorean -> 
-                    [{A, B, C} | triplets_with_sum(Sum, A + 2)];
+            Not_Pythagorean = R =/= 0 orelse A - B >= 0,
+            if Not_Pythagorean -> 
+                    triplets_with_sum(Sum, A + 1); % continue
                true -> 
-                    triplets_with_sum(Sum, A + 1) % continue
+                    [{A, B, C} | triplets_with_sum(Sum, A + 2)]
             end;
         false -> []
 	end.
