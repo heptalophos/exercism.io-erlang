4e89fce48eb4604e67c68b9a43dd083864b3125d
Zero Delta <zerodeltahedge@gmail.com>
2022-11-25 20:08:46 +0200

    Reformat hamming.erl

--- hamming/src/hamming.erl
+++ hamming/src/hamming.erl
@@ -2,12 +2,13 @@
 
 -export([distance/2]).
 
--spec distance(string(),string()) -> non_neg_integer() | {error, atom()}.
+-spec distance(string(), string()) -> non_neg_integer() | {error, atom()}.
 distance(Strand1, Strand2) -> 
     case (length(Strand1) =/= length(Strand2)) of 
-        true  -> {error, badarg};
-        false -> lists:sum(
-                    [1 || {N1, N2} <- lists:zip(Strand1, Strand2), 
-                                      N1 =/= N2 ]
-                 )
+        true  -> 
+            {error, badarg};
+        false -> 
+            lists:sum([1 || {N1, N2} <- lists:zip(Strand1, Strand2), 
+                                        N1 =/= N2 ]
+            )
     end.
