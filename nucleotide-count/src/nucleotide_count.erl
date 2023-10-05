bd3132ef1615957b2eb79fbd5aea35341c90ed0d
Zero Delta <zerodeltahedge@gmail.com>
2023-10-05 20:48:41 +0300

    Reformat nucleotide_count.erl

--- nucleotide-count/src/nucleotide_count.erl
+++ nucleotide-count/src/nucleotide_count.erl
@@ -6,19 +6,14 @@
 -type strand() :: [nucleotide()].
 -type nuc_count() :: {nucleotide(), non_neg_integer()}.
 
-
 -spec count(strand(), nucleotide()) -> non_neg_integer().
 count(Strand, Nucleotide) -> 
-    string:length(lists:filter(fun(X) -> 
-                                valid([X]) =:= Nucleotide 
-                               end, 
+    string:length(lists:filter(fun(X) -> valid([X]) =:= Nucleotide end, 
                                Strand)).
 
 -spec nucleotide_counts(strand()) -> [nuc_count()].
 nucleotide_counts(Strand) -> 
-    lists:map(fun(X) -> 
-                    {X, count(Strand, X)} 
-              end, 
+    lists:map(fun(X) -> {X, count(Strand, X)} end, 
               ["A", "C", "G", "T"]).
 
 % Auxiliary
