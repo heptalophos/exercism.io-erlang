ba8fc54861c28be6de24b4db7ec44776c0e874dd
Zero Delta <zerodeltahedge@gmail.com>
2023-10-05 20:48:22 +0300

    Reformat nth_prime.erl

--- nth-prime/src/nth_prime.erl
+++ nth-prime/src/nth_prime.erl
@@ -2,19 +2,17 @@
 
 -export([prime/1]).
 
--spec prime(pos_integer()) -> 
-                pos_integer()|{error, string()}.
-prime(N) -> case N of
-                0 -> error("badarg 0");
-                1 -> 2;
-                _ -> prime(N, 1)
-            end.
-
+-spec prime(pos_integer()) -> pos_integer()|{error, string()}.
+prime(N) -> 
+    case N of
+        0 -> error("badarg 0");
+        1 -> 2;
+        _ -> prime(N, 1)
+    end.
 
 % Auxiliary Functions
 
--spec prime(pos_integer(), pos_integer()) -> 
-                                    pos_integer().
+-spec prime(pos_integer(), pos_integer()) -> pos_integer().
 prime(N, C) -> 
     Truth = fun (X) -> 
                 case X of 
@@ -23,10 +21,8 @@ prime(N, C) ->
                 end 
             end,
     case N of 
-        1 -> 
-            C;
-        _ -> 
-            prime(N - Truth(is_prime(C + 2)), C + 2)
+        1 -> C;
+        _ -> prime(N - Truth(is_prime(C + 2)), C + 2)
     end.
     
 -spec is_prime(pos_integer()) -> boolean().
@@ -34,9 +30,6 @@ is_prime(N) ->
     case N of 
         1 -> false;
         2 -> true;
-        _ -> lists:all(fun(X) -> 
-                        N rem X =/= 0 
-                       end, 
-                       lists:seq(2, 
-                                 ceil(math:sqrt(N))))
+        _ -> lists:all(fun(X) -> N rem X =/= 0 end, 
+                       lists:seq(2, ceil(math:sqrt(N))))
     end.
