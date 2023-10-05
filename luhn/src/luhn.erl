78a8ce78bde378ba97c4045c8431c96f338aac73
Zero Delta <zerodeltahedge@gmail.com>
2023-10-05 20:46:39 +0300

    Refactor luhn

--- luhn/src/luhn.erl
+++ luhn/src/luhn.erl
@@ -1,28 +1,31 @@
 -module(luhn).
-
 -author("heptalophos").
 
 -export([valid/1]).
 
 -spec valid(string()) -> boolean().
-valid(Value) -> 
-    valid(lists:reverse(Value), 1, 0).
+valid(Value) -> valid(lists:reverse(Value), 1, 0).
 
 % Auxiliary
 
 -spec valid(string(), integer(), integer()) -> boolean().
-valid([], Pos, CSum) ->
-    case Pos == 1 orelse Pos == 2 of
-        true -> false;
-        false -> CSum rem 10 =:= 0
-    end;
-valid([D|N], Pos, CSum) ->
-    case D of
-        $ -> 
-            valid(N, Pos, CSum);
-        X when X >= $0 andalso X =< $9 ->
-            valid(N, Pos + 1, CSum + adjust(D - $0, Pos));
-        _ -> false
+valid(String, Pos, CSum) ->
+    case String of
+        [] -> 
+            case Pos == 1 orelse Pos == 2 of
+                true -> false;
+                false -> CSum rem 10 =:= 0
+            end;
+        [D|N] ->
+            case D of
+                $ -> valid(N, Pos, CSum);
+                X -> 
+                    if  
+                        X >= $0 andalso X =< $9 -> 
+                            valid(N, Pos + 1, CSum + adjust(D - $0, Pos));
+                        true -> false
+                    end
+            end
     end. 
 
 -spec adjust(pos_integer(), pos_integer()) -> pos_integer().
