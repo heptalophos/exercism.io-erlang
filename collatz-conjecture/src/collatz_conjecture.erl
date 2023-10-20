971444cd4cf6f75d41278da1e04da8d125278631
Zero Delta <zerodeltahedge@gmail.com>
2023-10-20 23:00:32 +0300

    Reformat collatz_conjecture.erl

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -5,10 +5,8 @@
 -spec steps(integer()) -> non_neg_integer() | error.
 steps(N) ->
     case is_integer(N) andalso N > 0 of
-        true -> 
-            steps(N, 0);
-        _ -> 
-            error(badarg)
+        true -> steps(N, 0);
+        _    -> error(badarg)
     end.
 
 
@@ -17,13 +15,9 @@ steps(N) ->
 -spec steps(N::integer(), Step::non_neg_integer()) -> non_neg_integer().
 steps(N, Step) -> 
     case N of 
-        1 -> 
-            Step;
-        _ -> 
-            if 
-                N band 1 =:= 0 -> 
-                        steps(N bsr 1, Step + 1);
-                true -> 
-                        steps(3 * N + 1, Step + 1)
-            end
+        1 -> Step;
+        _ -> if 
+                N band 1 =:= 0 -> steps(N bsr 1, Step + 1);
+                true -> steps(3 * N + 1, Step + 1)
+             end
     end.
