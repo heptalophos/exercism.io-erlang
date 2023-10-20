b08bd805546808b31fb04fec2fae7201133f20a8
Zero Delta <zerodeltahedge@gmail.com>
2023-10-20 23:02:07 +0300

    Refactor custom-set

--- custom-set/src/custom_set.erl
+++ custom-set/src/custom_set.erl
@@ -12,77 +12,75 @@
 
 -spec contains(any(), custom_set()) -> boolean().
 contains(_, Set) when Set#custom_set.elements == [] -> false;
-contains(Elem, Set) -> #custom_set{elements = Elements} = Set,
+contains(Elem, Set) -> #custom_set {elements = Elements} = Set,
 		[H|T] = Elements,
 		case H of
 			Elem -> true;
-			_ -> contains(Elem, #custom_set{elements = T})
+			_ -> contains (Elem, #custom_set {elements = T})
 		end.
 
 -spec add(any(), custom_set()) -> custom_set().
 add(Elem, Set) -> #custom_set{elements = Elems} = Set,
-		case contains(Elem, Set) of
+		case contains (Elem, Set) of
 			true -> Elements = Elems;
 			false -> Elements = [Elem|Elems]
 		end,
-		#custom_set{elements = Elements}.
+		#custom_set {elements = Elements}.
 
 -spec difference(custom_set(), custom_set()) -> custom_set().
-difference(Set1, Set2) -> #custom_set{elements = Elements1} = Set1,
-						  #custom_set{elements = Elements2} = Set2,
-						  #custom_set{elements = Elements1 -- Elements2}.
+difference(Set1, Set2) -> 
+	#custom_set {elements = Elements1} = Set1,
+	#custom_set {elements = Elements2} = Set2,
+	#custom_set {elements = Elements1 -- Elements2}.
 
 -spec disjoint(custom_set(), custom_set()) -> boolean().
-disjoint(Set1, Set2) -> #custom_set{elements = Elements1} = Set1,
-				difference(Set1, Set2) == #custom_set{elements = Elements1}.
+disjoint(Set1, Set2) -> 
+	#custom_set {elements = Elements1} = Set1,
+	difference (Set1, Set2) == #custom_set {elements = Elements1}.
 
 -spec empty(custom_set()) -> boolean().
 empty(Set) -> Set#custom_set.elements == [].
 
 -spec equal(custom_set(), custom_set()) -> boolean().
-equal(Set1, Set2) -> difference(Set1, Set2) == #custom_set{elements = []} 
-					 andalso 
-					 difference(Set2, Set1) == #custom_set{elements = []}.
+equal(Set1, Set2) -> 
+	difference (Set1, Set2) == #custom_set {elements = []} 
+	andalso 
+	difference (Set2, Set1) == #custom_set {elements = []}.
 
 -spec from_list(list()) -> custom_set().
-from_list(List) -> #custom_set{elements = 
-								lists:foldl(
-									fun(Elem, Acc) -> [Elem] ++ Acc end, 
-									[], 
-									List)
-							  }.
+from_list(List) -> #custom_set {elements = 
+									lists:foldl ( 
+										fun(Elem, Acc) -> [Elem] ++ Acc end, 
+										[], List )
+							   }.
 
 -spec intersection(custom_set(), custom_set()) -> custom_set().
-intersection(Set1, Set2) -> #custom_set{elements = Elements1} = Set1,
-							#custom_set{elements = Elements2} = Set2,
-							#custom_set{elements = lists:filter(fun(Elem) -> 
-					 								contains(Elem, 
-					 		 						#custom_set{elements = Elements2
-										}) 
-		             					end, 
-					 					Elements1)}.
+intersection(Set1, Set2) -> 
+	#custom_set {elements = Elements1} = Set1,
+	#custom_set {elements = Elements2} = Set2,
+	#custom_set {
+		elements = lists:filter (
+						fun (Elem) -> 
+							contains( Elem, 
+					 		 		  #custom_set {elements = Elements2} ) 
+		             	end, Elements1)}.
 
 -spec subset(custom_set(), custom_set()) -> boolean().
 subset(Set1, Set2) -> 
-	#custom_set{elements = Elements1} = Set1,
-	#custom_set{elements = Elements2} = Set2,
-	lists:all(fun(Elem) -> 
-				contains(Elem, 
-						 #custom_set{elements = Elements2}) 
-			  end, 
+	#custom_set {elements = Elements1} = Set1,
+	#custom_set {elements = Elements2} = Set2,
+	lists:all (fun (Elem) -> 
+				 contains(Elem, #custom_set{elements = Elements2}) end, 
 			  Elements1).
 
 -spec union(custom_set(), custom_set()) -> custom_set().
 union(Set1, Set2) -> 
-	#custom_set{elements = Elements1} = Set1,
-	#custom_set{elements = Elements2} = Set2,
-	#custom_set{elements = dedup(lists:foldl(fun(Elem, Acc) -> 
-													[Elem] ++ Acc 
-						  					 end, 
-						  					 Elements1, 
-						  					 Elements2))}.
+	#custom_set {elements = Elements1} = Set1,
+	#custom_set {elements = Elements2} = Set2,
+	#custom_set {elements = dedup(lists:foldl (fun (Elem, Acc) -> [Elem] ++ Acc 
+						  					   end, Elements1, Elements2))}.
 
 %Auxiliary 
 
 -spec dedup(list()) -> list().
-dedup(List) -> ordsets:from_list(ordsets:to_list(List)).
+dedup(List) -> ordsets:from_list (ordsets:to_list (List)).
