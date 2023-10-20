85e7868f48663597917a0b59987ba715019c218c
Zero Delta <zerodeltahedge@gmail.com>
2023-10-20 23:01:37 +0300

    Reformat darts.erl

--- darts/src/darts.erl
+++ darts/src/darts.erl
@@ -4,15 +4,10 @@
 
 -spec score(number(), number()) -> integer().
 score(X, Y) -> 
-    Radius = 
-        math:sqrt(math:pow(X, 2) + math:pow(Y, 2)),
+    Radius = math:sqrt(math:pow(X, 2) + math:pow(Y, 2)),
     if 
-        Radius =< 1 -> 
-            10;
-        Radius =< 5 -> 
-            5;
-        Radius =< 10 -> 
-            1;
-        true -> 
-            0
+        Radius =< 1 -> 10;
+        Radius =< 5 -> 5;
+        Radius =< 10 ->1;
+        true -> 0
     end.
