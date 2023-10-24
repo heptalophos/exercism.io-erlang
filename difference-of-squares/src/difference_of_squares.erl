9ba977ecdbc6b4acca199afd33a05bb225300f6f
Zero Delta <zerodeltahedge@gmail.com>
2023-10-24 14:28:36 +0300

    Reformat difference-of-squares

--- difference-of-squares/src/difference_of_squares.erl
+++ difference-of-squares/src/difference_of_squares.erl
@@ -10,17 +10,16 @@
 square(X) -> X * X.
 
 -spec square_of_sum(integer()) -> integer(). 
-square_of_sum(Number) ->
-    square(lists:foldl(fun(X, Sum) -> 
-        X + Sum end, 0, lists:seq(1, Number))).
+square_of_sum(Number) -> 
+    square (lists:foldl (fun (X, Sum) -> X + Sum end, 
+                         0, 
+                        lists:seq(1, Number))).
 
 -spec sum_of_squares(integer()) -> integer(). 
 sum_of_squares(Number) -> 
-    lists:foldl(fun(X, Sum) -> 
-        square(X) + Sum end, 0, lists:seq(1, Number)).
+    lists:foldl (fun (X, Sum) -> square(X) + Sum end, 
+                 0, lists:seq(1, Number)).
 
 -spec difference_of_squares(integer()) -> integer(). 
 difference_of_squares(Number) -> 
     square_of_sum(Number) - sum_of_squares(Number).
-
-
