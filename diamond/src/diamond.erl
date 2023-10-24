5e05660c17a67482b66ff1327d8a32547902480c
Zero Delta <zerodeltahedge@gmail.com>
2023-10-24 14:56:19 +0300

    Update diamond.erl

--- diamond/src/diamond.erl
+++ diamond/src/diamond.erl
@@ -2,42 +2,32 @@
 
 -export([rows/1]).
 
+-spec rows([$A..$Z]) -> [[$A..$Z | $ ], ...].
+rows ([Letter]) -> diamond (Letter, 0, 2 * (Letter - $A) - 1, []).
+
+%% Auxiliary functions
 
 -spec spaces(non_neg_integer()) -> [$ ,...].
-spaces (0) -> "";
 spaces (N) -> 
-    [" " || _ <- lists:seq(1, N) ].
+    case N of
+        0 -> "";
+        _ -> [" " || _ <- lists:seq(1, N) ]
+    end.
 
+-spec row([$A..$Z], integer(), integer()) -> [[$A..$Z | $ ],...].
+row(Letter, Outside, Middle) ->
+    case Middle > 0 of
+        true -> lists:flatten ([spaces (Outside), Letter, spaces (Middle), 
+                                Letter, spaces (Outside)]);
+        _ -> lists:flatten ([spaces (Outside), Letter, spaces (Outside)])
+    end.
 
--spec row([$A..$Z], integer(), integer()) -> 
-                               [[$A..$Z | $ ],...].
-row(Letter, Outside, Middle) when Middle > 0 -> 
-    lists:flatten([spaces(Outside), 
-                   Letter, 
-                   spaces(Middle), 
-                   Letter, 
-                   spaces(Outside)]);
-row(Letter, Outside, _) -> 
-    lists:flatten([spaces(Outside), 
-                   Letter, 
-                   spaces(Outside)]).
-
-
--spec diamond([$A..$Z],integer(),integer(),[[$A..$Z | $ ],...]) -> 
-                                           [[$A..$Z | $ ],...].
-diamond(Letter, Outside, Middle, Acc) when Middle < 1 ->
-        Upper = 
-            [row(Letter, Outside, Middle) | Acc],
-        [_|Lower] = 
-            lists:reverse(Upper),
-        Upper ++ Lower;
-diamond(Letter, Outside, Middle, Acc) ->
-    diamond(Letter - 1, 
-            Outside + 1, 
-            Middle - 2, 
-            [row(Letter, Outside, Middle) | Acc]).
-
-
--spec rows([$A..$Z]) -> [[$A..$Z | $ ], ...].
-rows([Letter]) -> 
-    diamond(Letter, 0, 2 * (Letter - $A) - 1, []).
+-spec diamond([$A..$Z],integer(),integer(),[[$A..$Z | $ ],...]) -> [[$A..$Z | $ ],...].
+diamond (Letter, Outside, Middle, Acc) ->
+    case Middle < 1 of 
+        true -> Upper     = [row(Letter, Outside, Middle) | Acc],
+                [_|Lower] = lists:reverse(Upper),
+                Upper ++ Lower;
+        _ -> diamond (Letter - 1, Outside + 1, Middle - 2, 
+                      [row(Letter, Outside, Middle) | Acc])
+    end.
