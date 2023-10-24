bbb73306f2d5690aff651b73e06250d749fa5a2b
Zero Delta <zerodeltahedge@gmail.com>
2023-10-24 14:55:08 +0300

    Update isogram.erl

--- isogram/src/isogram.erl
+++ isogram/src/isogram.erl
@@ -7,6 +7,9 @@ is_isogram(Phrase) ->
     Cs = [C || C <- string:to_lower(Phrase), $a =< C, C =< $z],
     has_duplicates(lists:sort(Cs)) == false.
 
+
+%% Auxiliary 
+
 -spec has_duplicates([any()]) -> boolean().
 has_duplicates([X, Y | _]) when X =:= Y -> true;
 has_duplicates([_ | Rest]) -> has_duplicates(Rest);
