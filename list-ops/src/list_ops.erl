a1d851ca6e08004c0c8796f9f1523c3c3a802fa6
Zero Delta <zerodeltahedge@gmail.com>
2023-10-24 14:29:32 +0300

    Refactor list-ops

--- list-ops/src/list_ops.erl
+++ list-ops/src/list_ops.erl
@@ -5,56 +5,54 @@
 -export([append/2, concat/1, filter/2, length/1, 
 		 map/2, foldl/3, foldr/3, reverse/1]).
 
--type pred() :: fun((any()) -> boolean()).
+-type predicate() :: fun((any()) -> boolean()).
 -type transform() :: fun((any()) -> any()).
 -type injection() :: fun((any(), any()) -> any()).
 
--spec append(list(), list()) -> list().
-append(Xs, Ys) ->
-	foldr(fun(X, Acc) -> [X | Acc] end, Ys, Xs).
 
--spec concat(list()) -> list().
-concat(Ls) ->
-	foldl(fun(Acc, Xs) -> append(Xs, Acc) end, [], Ls).
+-spec foldl (injection(), any(), list()) -> any().
+foldl (Fn, Acc, List) ->
+	case List of
+		[] -> Acc;
+		[H | Ts] -> foldl ( Fn, Fn(H, Acc), Ts )
+	end.
 
--spec filter(pred(), list()) -> list().
-filter(Pred, Ls) ->
-	foldl(	fun(L, Acc) -> 
+-spec foldr (injection(), any(), list()) -> any().
+foldr (Fn, Acc, List) -> 
+	case List of
+		[] -> Acc;
+		[H | Ts] -> Fn ( H, foldr (Fn, Acc, Ts) )
+	end.
+
+% all other ops, are defined in terms of foldl or foldr, 
+% using appropriate (mostly "append/2"-based) injections
+
+-spec append (list(), list()) -> list().
+append (Xs, Ys) -> foldr ( fun (X, Acc) -> [X | Acc] end, Ys, Xs ).
+
+-spec concat (list()) -> list().
+concat (List) -> foldl ( fun (Acc, Xs) -> append (Xs, Acc) end, [], List ).
+
+-spec filter (predicate(), list()) -> list().
+filter (Pred, List) ->
+	foldl (	fun (L, Acc) -> 
 				case Pred(L) of 
-					true -> append(Acc, [L]);
+					true  -> append (Acc, [L]);
 					false -> Acc
 				end
 		 	end, 
-		 	[], Ls	). 
+		 	[], List ). 
 
--spec length(list()) -> non_neg_integer().
-length(Ls) -> 
-	foldl(fun(_, Acc) -> Acc + 1 end, 0, Ls).
+-spec length (list()) -> non_neg_integer().
+length (List) -> foldl (fun (_, Acc) -> Acc + 1 end, 0, List).
 
--spec map(transform(), list()) -> list().
-map(_, []) -> [];
-map(Transform, Ls) ->
-	foldl(	fun(L, Acc) -> 
-				append(Acc, [Transform(L)]) 
-		  	end, 
-		  	[], Ls	).
-
--spec foldl(injection(), any(), list()) -> any().
-foldl(Fn, Acc, Ls) ->
-	case Ls of
-		[] -> Acc;
-		[H | Ts] -> foldl(Fn, Fn(H, Acc), Ts)
+-spec map (transform(), list()) -> list().
+map (Transform, List) ->
+	case List of
+		[] -> [];
+		_  -> foldl ( fun (L, Acc) -> append (Acc, [Transform(L)]) end, 
+		  	         [], List )
 	end.
 
--spec foldr(injection(), any(), list()) -> any().
-foldr(Fn, Acc, Ls) -> 
-	case Ls of
-		[] -> Acc;
-		[H | Ts] -> Fn(H, foldr(Fn, Acc, Ts))
-	end.
-
--spec reverse(list()) -> list().
-reverse(Ls) -> 
-	foldl(fun(L, Init) -> [L | Init] end, [], Ls).
-
-%% Auxiliary
+-spec reverse (list()) -> list().
+reverse (List) -> foldr (fun (L, Acc) -> append (Acc, [L]) end, [], List).
