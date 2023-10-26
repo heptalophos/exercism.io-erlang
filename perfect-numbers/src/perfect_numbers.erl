4e6ed6c4bc2f252e4c5373388f8818e3efc34a65
Zero Delta <zerodeltahedge@gmail.com>
2023-10-27 02:28:48 +0300

    Refactor perfect-numbers

--- perfect-numbers/src/perfect_numbers.erl
+++ perfect-numbers/src/perfect_numbers.erl
@@ -2,19 +2,18 @@
 
 -export([classify/1]).
 
--type nicomachusclass() :: perfect 
-                           | abundant 
-                           | deficient.
+-type nicomachusclass() :: perfect | abundant | deficient.
 
 -spec classify(pos_integer()) -> nicomachusclass() | error.
-classify(Number) when Number > 0 ->
-    Factors = [Factor || Factor <- lists:seq(1, Number div 2), 
-                                   Number rem Factor == 0],
-    AliquotSum = lists:sum(Factors),
-    if 
-        AliquotSum >  Number -> abundant;
-        AliquotSum == Number -> perfect;
-        AliquotSum <  Number -> deficient
-    end;
-classify(_) -> 
-    error("Not a positive integer").
+classify(Number) ->
+    case Number > 0 of
+        true -> Factors = [Factor || Factor <-  lists:seq(1, Number div 2), 
+                                                Number rem Factor == 0],
+                AliquotSum = lists:sum(Factors),
+                if 
+                    AliquotSum >  Number -> abundant;
+                    AliquotSum == Number -> perfect;
+                    AliquotSum <  Number -> deficient
+                end;
+        _ -> error("Not a positive integer")
+    end.
