2ad0c54225474ade74e7a8816aff6dca568ff240
Zero Delta <zerodeltahedge@gmail.com>
2024-01-24 23:55:11 +0200

    Update rail_fence_cipher.erl

--- rail-fence-cipher/src/rail_fence_cipher.erl
+++ rail-fence-cipher/src/rail_fence_cipher.erl
@@ -2,7 +2,8 @@
 
 -export([decode/2, encode/2]).
 
-
+-spec decode(string(), pos_integer()) -> string().
 decode(_Message, _Rails) -> undefined.
 
+-spec encode(string(), pos_integer()) -> string().
 encode(_Message, _Rails) -> undefined.
