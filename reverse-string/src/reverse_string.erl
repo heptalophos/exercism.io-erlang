6fcd03e0eb029eaa17dc4e88d5a43c0c03c37198
Zero Delta <zerodeltahedge@gmail.com>
2024-02-16 01:26:02 +0200

    reverse-string - 1st passing iter

--- reverse-string/src/reverse_string.erl
+++ reverse-string/src/reverse_string.erl
@@ -2,5 +2,15 @@
 
 -export([reverse/1]).
 
-reverse(String) ->
-    undefined.
+-spec reverse([char()]) -> [char()].
+reverse(String) -> 
+    reverse(String, []).
+
+% Auxiliary
+
+-spec reverse([char()], [char()]) -> list(char()).
+reverse(String, Reversed) ->
+    case String of
+        [] -> Reversed;
+        [H | T] -> reverse(T, [H | Reversed])   
+    end.
