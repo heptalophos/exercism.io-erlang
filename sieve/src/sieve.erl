47b971891a41dc110ea1df87e5d16c8afea6703a
Zero Delta <zerodeltahedge@gmail.com>
2024-02-23 16:05:22 +0200

    Refactor sieve.erl

--- sieve/src/sieve.erl
+++ sieve/src/sieve.erl
@@ -4,17 +4,21 @@
 
 
 -spec primes(Limit) -> [Prime] 
-                when Limit :: pos_integer(), Prime :: pos_integer().
+        when Limit :: pos_integer(), 
+             Prime :: pos_integer().
 primes(Limit) -> 
-        Nums = lists:seq(2, Limit),
-        sieve(Nums, [], Limit).
+    Nums = lists:seq(2, Limit),
+    sieve(Nums, [], Limit).
 
 % Auxiliary
 
 -spec sieve([Number], [Prime], Limit) -> [Prime] 
-      when Number :: pos_integer(), Prime  :: pos_integer(), Limit :: pos_integer(). 
-sieve([], Primes, _) ->
-        lists:reverse(Primes);
-sieve([Num|Nums], Primes, Limit) ->
-        Comp = lists:seq(2 * Num, Limit, Num),
-        sieve(Nums -- Comp, [Num|Primes], Limit).
+      when Number :: pos_integer(), 
+           Prime  :: pos_integer(), 
+           Limit  :: pos_integer(). 
+sieve(Numbers, Primes, _Limit) -> 
+    case Numbers of 
+        [] -> lists:reverse(Primes);
+        [Num|Nums] -> Comps = lists:seq(2 * Num, _Limit, Num),
+                      sieve(Nums -- Comps, [Num|Primes], _Limit)
+    end.
