9e4f81ca412b04d9a1e0d927af42bab3deca4e2b
Zero Delta <zerodeltahedge@gmail.com>
2024-06-11 16:13:31 +0300

    Update change.erl

--- change/src/change.erl
+++ change/src/change.erl
@@ -2,5 +2,15 @@
 
 -export([find_fewest_coins/2]).
 
+-spec find_fewest_coins(pos_integer(), [pos_integer()]) -> [pos_integer()].
+% find_fewest_coins(_Target, _Coins) -> .
+find_fewest_coins(0, _) -> [];
+find_fewest_coins(Target, _) when Target < 0 -> error(undefined);
+find_fewest_coins(Target, Coins) -> 
+    SortDenominations = lists:sort(fun (X, Y) -> X > Y end, Coins),
+    case Target < lists:min(Coins) of
+        true -> undefined;
+        _ -> find_fewest_coins(Target, SortDenominations, [])
+    end.
 
-find_fewest_coins(_Target, _Coins) -> undefined.
+#
