103e80c1636c435e57a6da0e3bd82cf14afb8705
Zero Delta <zerodeltahedge@gmail.com>
2025-02-25 22:58:02 +0200

    Reformat resistor_color

--- resistor-color/src/resistor_color.erl
+++ resistor-color/src/resistor_color.erl
@@ -2,17 +2,31 @@
 
 -export([colors/0, color_code/1]).
 
--type resistor_color() :: black | brown | red | orange | yellow 
-                        | green | blue | violet | grey | white.
+-type resistor_color() ::  black 
+                         | brown 
+                         | red 
+                         | orange 
+                         | yellow 
+                         | green 
+                         | blue 
+                         | violet 
+                         | grey 
+                         | white.
 
 -spec colors() -> list(resistor_color()).
-colors() ->
-    [black, brown, red, orange, yellow,
-     green, blue, violet, grey, white].
+colors() -> [ black
+            , brown
+            , red
+            , orange
+            , yellow
+            , green
+            , blue
+            , violet
+            , grey
+            , white].
 
 -spec color_code(resistor_color()) -> 0..9 | {error, atom()}. 
-color_code(Color) ->
-    color_code(Color, colors(), 0).
+color_code(Color) -> color_code(Color, colors(), 0).
 
 % Auxiliary
 
@@ -20,11 +34,9 @@ color_code(Color) ->
                  non_neg_integer()) -> 0..9 | {error, atom()}.
 color_code(C, Cs, Val) ->
     case Cs of
-        [] -> 
-            {error, color_not_found};
-        [H|T] -> 
-            if 
-                C =:= H -> Val;
-                true -> color_code(C, T, Val + 1)
-            end
+        []    -> {error, color_not_found};
+        [H|T] -> if 
+                    C =:= H -> Val;
+                    true -> color_code(C, T, Val + 1)
+                 end
     end.
