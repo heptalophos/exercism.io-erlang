eba681d251503e7b5abfaeb5fce0de05b8cf216d
Zero Delta <zerodeltahedge@gmail.com>
2025-02-25 22:59:38 +0200

    Refactor & retest queen_attack.erl

--- queen-attack/src/queen_attack.erl
+++ queen-attack/src/queen_attack.erl
@@ -17,8 +17,8 @@ can_attack(WhiteQueen, BlackQueen) ->
         true  -> %% any queen @ illegal rank or file
             {error, invalid_position_offboard};
         false -> %% both queens @ legal rank & file
-            case ({abs(WX - BX), abs(WY - BY)}) of 
-                {0, 0} -> {error, invalid_position_same}; %% both @ same square
+            case ({abs(WX - BX), abs(WY - BY)}) of
+                {0, 0} -> {error, honey_get_off}; %% both @ same square
                 {_, 0} -> true; %% both queens @ same file
                 {0, _} -> true; %% both queens @ same rank
                 {X, Y} -> X =:= Y %% queens @ same diagonal
